b32853ab2de27936cb052f03835b9f416fc5af46
Marko Sreckovic <markosrecko97@hotmail.rs>
2018-02-11 08:43:35 +0100

    No map borders

--- src/game/model/Piece.java
+++ src/game/model/Piece.java
@@ -8,20 +8,28 @@ public class Piece{
 		this.position = position;
 	}
 	
-	public Piece(Direction direction,Position position){
+	public Piece(Direction direction,Position position,Game game){
 		
 		switch(direction){
 		case UP:
 			this.position = new Position(position.getX(),position.getY()-1);
+			if(this.position.getY() == -1) 
+				this.position.setY(game.getDimension().getY()-1);
 			break;
 		case DOWN:
 			this.position = new Position(position.getX(),position.getY()+1);
+			if(this.position.getY() == game.getDimension().getY())
+				this.position.setY(0);
 			break;
 		case LEFT:
 			this.position = new Position(position.getX()-1,position.getY());
+			if(this.getPosition().getX() == -1)
+				this.position.setX(game.getDimension().getX()-1);
 			break;
 		case RIGHT:
 			this.position = new Position(position.getX()+1,position.getY());
+			if(this.position.getX() == game.getDimension().getX())
+				this.position.setX(0);
 			break;
 		}
 	}
