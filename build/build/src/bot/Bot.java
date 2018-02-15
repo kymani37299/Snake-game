6596f8af4b460e7bd2fa1b8f23c39c7ccaa116e1
Marko Sreckovic <markosrecko97@hotmail.rs>
2018-02-15 23:57:15 +0100

    build

--- build/build/src/bot/Bot.java
+++ build/build/src/bot/Bot.java
@@ -32,128 +32,126 @@ public class Bot implements GameController{
 		ArrayList<Position> snake = this.game.getSnake().getSnake();
 		Position head = this.game.getSnake().getPosition();
 		Position apple = this.game.getApple().getPosition();
-		int map[][] = new int[this.game.getDimension().getX()][this.game.getDimension().getY()];
-		for(int i=0;i<map.length;i++){
-			for(int j=0;j<map[0].length;j++){
-				map[i][j] = 0;
-				if(head.equals(new Position(i,j))){
-					map[i][j] = 1;
-				}else if(apple.equals(new Position(i,j))){
-					map[i][j] = 2;
-				}else{
-					Position pos = new Position(i,j);
-					for(Position p: snake){
-						if(p.equals(pos)){
-							map[i][j] = 3;
+		
+		int count = 0;
+		Position curr = new Position(head.getX(),head.getY());
+		boolean s = true;
+		boolean a = true;
+		do{
+			if(!curr.equals(head)){
+				if(a && curr.equals(apple)){
+					a = false;
+					input.set(0, 0, count);
+				}else if(s){
+					for(Position p : snake){
+						if(p.equals(curr)){
+							s = false;
+							input.set(0, 1, count);
 							break;
 						}
 					}
 				}
 			}
-		}
-		
-		//UP
-		int count = 0;
-		Position curr = new Position(head.getX(),head.getY());
-		boolean s = true;
-		boolean a = true;
-		for(int i=0;i<this.game.getDimension().getX();i++){
-			count++;
-			if(map[curr.getX()][curr.getY()]!=0){
-				if(s && map[curr.getX()][curr.getY()]==3){
-					input.set(0, 0, count);
-					s = false;
-				}else if(a && map[curr.getX()][curr.getY()]==2){
-					input.set(0, 4, count);
-					a = false;
-				}
-			}
 			if(!s && !a) break;
+			count++;
 			curr.incX();
 			if(curr.getX() == this.game.getDimension().getX()){
 				curr.setX(0);
 			}
-		}
-		if(s) input.set(0, 0, 0);
-		if(a) input.set(0, 4, 0);
+		}while(!curr.equals(head));
 		
-		//DOWN
+		if(a) input.set(0, 0, 0);
+		if(s) input.set(0, 1, 0);
+		
 		count = 0;
 		curr = new Position(head.getX(),head.getY());
 		s = true;
 		a = true;
-		for(int i=0;i<this.game.getDimension().getX();i++){
-			count++;
-			if(map[curr.getX()][curr.getY()]!=0){
-				if(s && map[curr.getX()][curr.getY()]==3){
-					input.set(0, 1, count);
-					s = false;
-				}else if(a && map[curr.getX()][curr.getY()]==2){
-					input.set(0, 5, count);
+		do{
+			if(!curr.equals(head)){
+				if(a && curr.equals(apple)){
 					a = false;
+					input.set(0, 2, count);
+				}else if(s){
+					for(Position p : snake){
+						if(p.equals(curr)){
+							s = false;
+							input.set(0, 3, count);
+							break;
+						}
+					}
 				}
 			}
 			if(!s && !a) break;
+			count++;
 			curr.decX();
 			if(curr.getX() == -1){
 				curr.setX(this.game.getDimension().getX()-1);
 			}
-		}
+		}while(!curr.equals(head));
 		
-		if(s) input.set(0, 1, 0);
-		if(a) input.set(0, 5, 0);
+		if(a) input.set(0, 2, 0);
+		if(s) input.set(0, 3, 0);
 		
-		//LEFT
 		count = 0;
 		curr = new Position(head.getX(),head.getY());
 		s = true;
 		a = true;
-		for(int i=0;i<this.game.getDimension().getX();i++){
-			count++;
-			if(map[curr.getX()][curr.getY()]!=0){
-				if(s && map[curr.getX()][curr.getY()]==3){
-					input.set(0, 2, count);
-					s = false;
-				}else if(a && map[curr.getX()][curr.getY()]==2){
-					input.set(0, 6, count);
+		do{
+			if(!curr.equals(head)){
+				if(a && curr.equals(apple)){
 					a = false;
+					input.set(0, 4, count);
+				}else if(s){
+					for(Position p : snake){
+						if(p.equals(curr)){
+							s = false;
+							input.set(0, 5, count);
+							break;
+						}
+					}
 				}
 			}
 			if(!s && !a) break;
+			count++;
 			curr.decY();
 			if(curr.getY() == -1){
 				curr.setY(this.game.getDimension().getY()-1);
 			}
-		}
+		}while(!curr.equals(head));
 		
-		if(s) input.set(0, 2, 0);
-		if(a) input.set(0, 6, 0);
+		if(a) input.set(0, 4, 0);
+		if(s) input.set(0, 5, 0);
 		
-		//RIGHT
 		count = 0;
 		curr = new Position(head.getX(),head.getY());
 		s = true;
 		a = true;
-		for(int i=0;i<this.game.getDimension().getX();i++){
-			count++;
-			if(map[curr.getX()][curr.getY()]!=0){
-				if(s && map[curr.getX()][curr.getY()]==3){
-					input.set(0, 3, count);
-					s = false;
-				}else if(a && map[curr.getX()][curr.getY()]==2){
-					input.set(0, 7, count);
+		do{
+			if(!curr.equals(head)){
+				if(a && curr.equals(apple)){
 					a = false;
+					input.set(0, 6, count);
+				}else if(s){
+					for(Position p : snake){
+						if(p.equals(curr)){
+							s = false;
+							input.set(0, 7, count);
+							break;
+						}
+					}
 				}
 			}
 			if(!s && !a) break;
+			count++;
 			curr.incY();
 			if(curr.getY() == this.game.getDimension().getY()){
 				curr.setY(0);
 			}
-		}
+		}while(!curr.equals(head));
 		
-		if(s) input.set(0, 3, 0);
-		if(a) input.set(0, 7, 0);
+		if(a) input.set(0, 6, 0);
+		if(s) input.set(0, 7, 0);
 		
 		return input;
 	}
